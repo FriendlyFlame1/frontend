25ae5eaf274778681a91ba0a7cd59212d37a9258
Friendlyflame1 <yash.goel@bntechnology.net>
2024-01-18 17:30:45 +0530

    connect

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   // Function to fetch data from the server
   const fetchData = async () => {
     try {
-      const response = await fetch('http://localhost:5000/api/data');
+      const response = await fetch('https://ill-plum-frog-robe.cyclic.app/api/data');
       const jsonData = await response.json();
 
       // Update state with the fetched data
